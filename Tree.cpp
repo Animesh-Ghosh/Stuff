25ba8e1f3a4ce1bfec57039ee7cf854a820e8f69
Animesh Ghosh <34956994+Animesh-Ghosh@users.noreply.github.com>
2018-09-24 17:29:08 +0530

    Uploaded new version of Tree
    Added Search function for Tree

--- Tree.cpp
+++ Tree.cpp
@@ -30,37 +30,67 @@ NODE* Insert_Node (NODE *root, int& data) {
 	}
 }
 
+// delete function
+NODE* Delete_Node (NODE *root, int& data) {
+	// do something
+}
+
 // traverse tree
 void InOrder (NODE *root) {	// inorder traversal
 	 // made this on the first try!!! BOI!!!
 	 if (root->left != NULL) InOrder (root->left);
-	 cout<<root->data<<" ";
+	 cout << root->data<<" ";
 	 if (root->right != NULL) InOrder (root->right);
 }
+
 void PreOrder (NODE *root) { // preorder traversal. Thanks to BHups!
 	//NODE*cur=root;
 	if (root == NULL) return;
-	cout<<root->data<<" ";
+	cout << root->data << " ";
 	PreOrder (root->left);
 	PreOrder (root->right);
 }
+
+// search function
+int Exists (NODE *root, int& data) {
+	// do something
+	if (root->data == data) {
+		// cout << "Element found!" << endl;
+		return 1;
+	}
+	else {
+		if (root->left != NULL) Exists (root->left, data);
+		if (root->right != NULL) Exists (root->right, data);
+	}
+}
+
+void Search (NODE *root, int& data) { // using Exists function
+	int flag = 0;
+	flag = Exists (root, data);
+	if (flag != 0) cout << data << " found!" << endl;
+	else cout << data << " not found!" << endl;
+}
+
 
 // main
 int main () {
 	NODE *root = NULL;
 	//int datum[] = {2, 0, 1, 5, 3, 4}; //sample input: 2, 0, 1, 5, 3, 4
 	int N, data;
-	cout<<"Enter number of elements to be inserted: ";
-	cin>>N;
+	cout << "Enter number of elements to be inserted: ";
+	cin >> N;
 	for (int i = 0; i < N ; i++) {
-		cout<<"Enter data element: ";
-		cin>>data;
+		cout << "Enter data element: ";
+		cin >> data;
 		root = Insert_Node (root, data);
 	}
 
-	cout<<"Tree in IN-ORDER traversal: "<<endl;
+	cout << "Tree in IN-ORDER traversal: " << endl;
 	InOrder (root);
-	cout<<"\nTree in PRE-ORDER traversal: "<<endl;
+	cout << "\nTree in PRE-ORDER traversal: " << endl;
 	PreOrder (root);
+	cout << "\nEnter element to be searched: ";
+	cin >> data;
+ 	Search (root, data);
 	return 0;
 }
