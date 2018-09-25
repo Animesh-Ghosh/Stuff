6bdeef827819e10eb4c1c62cfe3cfe2ed455d78c
Animesh Ghosh <34956994+Animesh-Ghosh@users.noreply.github.com>
2018-09-25 15:12:26 +0530

    Updated TestShift
    Removed unnecessary header file string.h

--- TestShift.c
+++ TestShift.c
@@ -1,10 +1,9 @@
 # include <stdio.h>
-# include <string.h>
 # include  <conio.h>
 
 int main () {
-	((-1 >> 1) < 0) ? puts ("Arithmetic Shift") : puts ("Logical Shift");
-	printf ("%d", ((-1 >> 1)));
+	printf ("-1 left-shifted once: %d\n", ((-1 >> 1)));
+	((-1 >> 1) < 0) ? printf ("Arithmetic Shift") : printf ("Logical Shift");
 	getch ();
 	return 0;
 }
