265a00bbebb07b3f2f720983537769d5b5c6814b
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2018-11-09 22:40:21 +0530

    Updated file via Upload
    Updated Prime.cpp:-
    -added moar loops.
    -getting closer (I think) to completely implementing Eratosthenes' sieve.

--- Prime.cpp
+++ Prime.cpp
@@ -8,13 +8,18 @@ using std::endl;
 
 // thought of using binary search in my mind
 int BinSearch (int &val, int Ar[], int &N) {
-	int low = 0, high = N - 1, mid;
-	while (low != high) {
+	int low = 0, high = N - 1, mid, flag = 0;
+	while (low <= high) {
 		mid = (low + high) / 2;
 		if (val < Ar[mid]) high = mid - 1;
 		else if (Ar[mid] < val) low = mid + 1;
-		else return 1;
+		else {
+			flag = 1;
+			break;
+		}
 	}
+	if (flag) return flag;
+	else return flag;
 }
 
 /* Sieve of Eratosthenes (for reference)
@@ -29,22 +34,57 @@ Otherwise, let p now equal this new number (which is the next prime), and repeat
 
 void Prime (int &N) {
 	int *Sieve = new int[N - 1];
-	for (int i = 2; i < (N + 1); i++) Sieve[i - 2] = i;
-	int prime = Sieve[0];
-	for (int j = prime; j < (N - 1); prime+=prime) {
-		if (Sieve[j] % prime == 0) {
-			Sieve[j] = 0;
+	cout << "Generating list..." << endl;
+	for (int i = 2; i < (N + 1); i++) Sieve[i - 2] = i; // generating list
+	
+	int prime = Sieve[0]; // initialising prime to 2
+	for (i = 0; i < (N - 1); i++) cout << Sieve[i] << " ";
+	
+	cout << endl << "Marking multiples of " << prime << "..." << endl;
+	for (i = prime; i < (N - 1); i += prime) // marking multiples of selected prime
+		if (Sieve[i] % prime == 0) Sieve[i] = 0;
+	for (i = 0; i < (N - 1); i++) cout << Sieve[i] << " ";
+	
+	int count = 0;
+	cout << endl << "Counting zero elements..." << endl;
+	for (i = 0; i < (N - 1); i++)  // counting zero elements
+		if (Sieve[i] == 0) count++;
+	cout << "Zero element count: " << count << endl;
+	
+	int *tSieve = new int[N - 1 - count];
+	int k = 0;
+	cout << "Creating sieved list..." << endl;
+	for (int j = 0; j < (N - 1); j++) {
+		if (Sieve[j] != 0) {
+			cout << "Found non-zero element! ";
+			tSieve[k] = Sieve[j];
+			cout << tSieve[k] << endl;
+			k++;
 		}
-		cout << Sieve[j] << " ";
 	}
+	for (int j = 0; j < (N - 1 - count); j++) cout << tSieve[j] << " ";
+	
+	cout << endl << "Selected prime: " << prime << endl;;
+	prime = tSieve[1];
+	cout << "New prime: " << prime << endl;
+	delete []Sieve;
+	Sieve = tSieve;
+	delete []tSieve;
+	cout << "Sieved list: " << endl;
+	for (i = 0; i < (N - 1 - count); i++) cout << Sieve[i] << " ";
+	int size = N - 1 - count;
+	//if (BinSearch (N, Sieve, size)) cout << "Number still in sieve!" << endl;
+	//else cout << "Number not prime!" << endl;
 }
 
 int main () {
-	int x;
-	cin >> x;
-	Prime (x);
-//	if (Prime (x)) cout << "Prime!" << endl;
-//	else cout << "Composite!" << endl;
+	
+//	int Ar[] = {2, 3, 4, 6, 7, 8, 10}, N = 7;
+//	for (int i = 0; i < N; i++) {
+//		if (BinSearch (Ar[i], Ar, N)) cout << Ar[i] << " found!" << endl;
+//		else cout << Ar[i] << " not found!" << endl;
+//	}
+	
 	getch ();
 	return 0;
 }
