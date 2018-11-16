0783e84b148460492e7f7e17013652b302b4fb72
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2018-11-16 14:13:02 +0530

    Removing files
    Restarting repo

--- TestShift.c
+++
@@ -1,9 +0,0 @@
-# include <stdio.h>
-# include  <conio.h>
-
-int main () {
-	printf ("-1 left-shifted once: %d\n", ((-1 >> 1)));
-	((-1 >> 1) < 0) ? printf ("Arithmetic Shift") : printf ("Logical Shift");
-	getch ();
-	return 0;
-}
