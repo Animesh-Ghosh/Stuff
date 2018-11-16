563007efd51a3d1bcd8e7f1103bdc0aac07b53f1
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2018-11-16 15:47:11 +0530

    Update Prime.cpp
    Primality test working.

--- Prime.cpp
+++ Prime.cpp
@@ -31,25 +31,25 @@ int BinSearch (int &val, int Ar[], int N) {
 }
 
 void Generate (int List[], int N) {
-	cout << "Generating list: " << endl;
+	// cout << "Generating list: " << endl;
 	for (int i = 0; i < N; i++) List[i] = i;
 	List[0] = List[1];
 }
 
 void Show (int List[], int N, int prime) {
 	for (int i = prime; i < N; i++) cout << List[i] << " ";
-	cout << endl;
+	// cout << endl;
 }
 
 void Mark (int List[], int N, int &prime) {
-	cout << "Marking multiples of " << prime << ": " << endl;
+	// cout << "Marking multiples of " << prime << ": " << endl;
 	for (int i = prime * prime; i < N; i += prime) 
 		if (List[i] % prime == 0) List[i] = 0;
 }
 
 int zeroCount (int List[], int N, int &prime) {
 	int count = 0;
-	cout << "Counting zero-elements: " << endl;
+	// cout << "Counting zero-elements: " << endl;
 	for (int i = prime; i < N; i++) 
 		if (List[i] == 0) count++;
 	return count;
@@ -57,7 +57,7 @@ int zeroCount (int List[], int N, int &prime) {
 
 void Generate (int Sieve[], int List[], int N, int &prime) {
 	int k = 0;
-	cout << "Generating sieved list: " << endl;
+	// cout << "Generating sieved list: " << endl;
 	for (int i = prime; i < N; i++) {
 		if (List[i] != 0) {
 			Sieve[k] = List[i];
@@ -70,30 +70,31 @@ void Prime (int &N) {
 	int *List = new int[N + 1];
 	Generate (List, (N + 1));
 	int prime = List[2];
-	Show (List, (N + 1), prime);
+	// Show (List, (N + 1), prime);
 	do {
 		Mark (List, (N + 1), prime);
-		Show (List, (N + 1), prime);
+		// Show (List, (N + 1), prime);
 		int count = 0;
 		count = zeroCount (List, (N + 1), prime);
-		cout << "Zero-element count: " << count << endl;
+		// cout << "Zero-element count: " << count << endl;
 		int *Sieve = new int[N - 1 - count];
 		Generate (Sieve, List, (N + 1), prime);
-		Show (Sieve, (N - 1 - count), 0);
-		if (BinSearch (N, Sieve, (N + 1))) {
-			cout << N << " possibly prime!" << endl;
-			cout << "Current prime: " << prime << endl;
+		// Show (Sieve, (N - 1 - count), 0);
+		if (BinSearch (N, Sieve, (N - 1 - count))) {
+			// cout << N << " possibly prime!" << endl;
+			// cout << "Current prime: " << prime << endl;
 			prime = Sieve[1];
-			cout << "New prime: " << prime << endl;
+			// cout << "New prime: " << prime << endl;
 			delete []Sieve;
 		}
 		else {
-			cout << N << " not prime!" << endl;
+			// cout << N << " not prime!" << endl;
 			delete []Sieve;
 			break;
 		}
 	}while (prime != N);
 	if (prime == N) cout << N << " is prime!" << endl;
+	else cout << N << " is not prime!" << endl;
 }
 
 int main () {
