b057840dbb7acf66d83e507316ffc6c9eb374fe4
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2018-11-16 15:19:32 +0530

    Update Prime.cpp
    Cleaned up code.

--- Prime.cpp
+++ Prime.cpp
@@ -1,13 +1,21 @@
 # include <iostream>
 # include <conio.h>
-# include <stdlib.h>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
-// thought of using binary search in my mind
-int BinSearch (int &val, int Ar[], int &N) {
+/*
+1. Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
+2. Initially, let p equal 2, the smallest prime number.
+3. Enumerate the multiples of p by counting to n from 2p in increments of p, and mark them in the list 
+(these will be 2p, 3p, 4p, ...; the p itself should not be marked).
+4. Find the first number greater than p in the list that is not marked. If there was no such number, stop.
+Otherwise, let p now equal this new number (which is the next prime), and repeat from step 3.
+5. When the algorithm terminates, the numbers remaining not marked in the list are all the primes below n.
+*/
+
+int BinSearch (int &val, int Ar[], int N) {
 	int low = 0, high = N - 1, mid, flag = 0;
 	while (low <= high) {
 		mid = (low + high) / 2;
@@ -22,69 +30,68 @@ int BinSearch (int &val, int Ar[], int &N) {
 	else return flag;
 }
 
-/* Sieve of Eratosthenes (for reference)
-1. Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
-2. Initially, let p equal 2, the smallest prime number.
-3. Enumerate the multiples of p by counting to n from 2p in increments of p, and mark them in the list 
-(these will be 2p, 3p, 4p, ...; the p itself should not be marked).
-4. Find the first number greater than p in the list that is not marked. If there was no such number, stop.
-Otherwise, let p now equal this new number (which is the next prime), and repeat from step 3.
-5. When the algorithm terminates, the numbers remaining not marked in the list are all the primes below n.
-*/
+void Generate (int List[], int N) {
+	cout << "Generating list: " << endl;
+	for (int i = 0; i < N; i++) List[i] = i;
+	List[0] = List[1];
+}
 
-void Prime (int &N) {
-	int *Sieve = new int[N - 1];
-	cout << "Generating list..." << endl;
-	for (int i = 2; i < (N + 1); i++) Sieve[i - 2] = i; // generating list
-	
-	int prime = Sieve[0]; // initialising prime to 2
-	for (i = 0; i < (N - 1); i++) cout << Sieve[i] << " ";
-	
-	cout << endl << "Marking multiples of " << prime << "..." << endl;
-	for (i = prime; i < (N - 1); i += prime) // marking multiples of selected prime
-		if (Sieve[i] % prime == 0) Sieve[i] = 0;
-	for (i = 0; i < (N - 1); i++) cout << Sieve[i] << " ";
-	
+void Show (int List[], int N, int prime) {
+	for (int i = prime; i < N; i++) cout << List[i] << " ";
+	cout << endl;
+}
+
+void Mark (int List[], int N, int &prime) {
+	cout << "Marking multiples of " << prime << ": " << endl;
+	for (int i = prime * prime; i < N; i += prime) 
+		if (List[i] % prime == 0) List[i] = 0;
+}
+
+int zeroCount (int List[], int N, int &prime) {
 	int count = 0;
-	cout << endl << "Counting zero elements..." << endl;
-	for (i = 0; i < (N - 1); i++)  // counting zero elements
-		if (Sieve[i] == 0) count++;
-	cout << "Zero element count: " << count << endl;
-	
-	int *tSieve = new int[N - 1 - count];
+	cout << "Counting zero-elements: " << endl;
+	for (int i = prime; i < N; i++) 
+		if (List[i] == 0) count++;
+	return count;
+}
+
+void Generate (int Sieve[], int List[], int N, int &prime) {
 	int k = 0;
-	cout << "Creating sieved list..." << endl;
-	for (int j = 0; j < (N - 1); j++) {
-		if (Sieve[j] != 0) {
-			cout << "Found non-zero element! ";
-			tSieve[k] = Sieve[j];
-			cout << tSieve[k] << endl;
+	cout << "Generating sieved list: " << endl;
+	for (int i = prime; i < N; i++) {
+		if (List[i] != 0) {
+			Sieve[k] = List[i];
 			k++;
 		}
 	}
-	for (int j = 0; j < (N - 1 - count); j++) cout << tSieve[j] << " ";
-	
-	cout << endl << "Selected prime: " << prime << endl;;
-	prime = tSieve[1];
+}
+
+void Prime (int &N) {
+	int *List = new int[N + 1];
+	int i;
+	Generate (List, (N + 1));
+	int prime = List[2];
+	Show (List, (N + 1), prime);
+	Mark (List, (N + 1), prime);
+	Show (List, (N + 1), prime);
+	int count = 0;
+	count = zeroCount (List, (N + 1), prime);
+	cout << "Zero-element count: " << count << endl;
+	int *Sieve = new int[N - 1 - count];
+	Generate (Sieve, List, (N + 1), prime);
+	Show (Sieve, (N - 1 - count), 0);
+	if (BinSearch (N, Sieve, (N + 1))) cout << N << " in list!" << endl;
+	else cout << N << " not in list!" << endl;
+	cout << "Current prime: " << prime << endl;
+	prime = Sieve[1];
 	cout << "New prime: " << prime << endl;
 	delete []Sieve;
-	Sieve = tSieve;
-	delete []tSieve;
-	cout << "Sieved list: " << endl;
-	for (i = 0; i < (N - 1 - count); i++) cout << Sieve[i] << " ";
-	int size = N - 1 - count;
-	//if (BinSearch (N, Sieve, size)) cout << "Number still in sieve!" << endl;
-	//else cout << "Number not prime!" << endl;
 }
 
 int main () {
-	
-//	int Ar[] = {2, 3, 4, 6, 7, 8, 10}, N = 7;
-//	for (int i = 0; i < N; i++) {
-//		if (BinSearch (Ar[i], Ar, N)) cout << Ar[i] << " found!" << endl;
-//		else cout << Ar[i] << " not found!" << endl;
-//	}
-	
+	int x;
+	cin >> x;
+	Prime (x);
 	getch ();
 	return 0;
 }
