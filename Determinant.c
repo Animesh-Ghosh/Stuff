e625e6baff71771eae07425133f4919930f77c8c
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2018-11-16 13:52:38 +0530

    Updating repository
    Updating with latest commits.

--- Determinant.c
+++ Determinant.c
@@ -8,44 +8,47 @@ void Show(int A[][MAXSIZE], int);
 int detA(int A[][MAXSIZE], int);
 void Minor(int A[][MAXSIZE], int M[][MAXSIZE], int, int, int);
 
-int main  () {
+int main() {
 	int A[MAXSIZE][MAXSIZE], size, det;
-	scanf ("%d", &size);
-	Enter (A, size);
-	det = detA (A, size);
-	printf ("%d", det);
-	getch ();
+	scanf("%d", &size);
+	Enter(A, size);
+	det = detA(A, size);
+	printf("%d", det);
+	getch();
 	return 0;
 }
 
-void Enter (int A[][MAXSIZE], int size) {
+void Enter(int A[][MAXSIZE], int size) {
 	int i, j;
-	for (i = 0; i < size; i++) {
-		for (j = 0; j < size; j++) {
-			scanf ("%d", &A[i][j]);
+	for (i=0; i<size; i++) {
+		for (j=0; j<size; j++) {
+			scanf("%d", &A[i][j]);
 		}
 	}
 }
 
-int detA (int A[][MAXSIZE], int size) {
+int detA(int A[][MAXSIZE], int size) {
 	int det = 0, i;
-	int M[size - 1][size - 1];
-	if (size == 1) return A[0][0];
+	int M[size-1][size-1];
+	if (size==1) return A[0][0];
 	else {
-		for (i = 0; i < size; i++) {
-			Minor(A, M, 0, i, size);
-			det = det + (pow (-1, i) * A[0][i] * detA (M, size-1));
+		for (i=0; i<size; i++) {
+			if (A[0][i] == 0) continue;
+			else {
+				Minor (A, M, 0, i, size);
+				det = det + (pow (-1, i) * A[0][i] * detA (M, size - 1));
+			}
 		}
 		return det;
 	}
 }
 
-void Minor (int A[][MAXSIZE], int M[][MAXSIZE], int I, int J, int size) {
-	int i, j, k = 0, l = 0;
-	for (i = 0; i < size; i++) {
-		if (i != I) {
-			for (j = 0; j < size; j++) {
-				if (j != J) {
+void Minor(int A[][MAXSIZE], int M[][MAXSIZE], int I, int J, int size) {
+	int i, j, k=0, l=0;
+	for (i=0; i<size; i++) {
+		if (i!=I) {
+			for (j=0; j<size; j++) {
+				if (j!=J) {
 					M[k][l] = A[i][j];
 					l++;
 				}
