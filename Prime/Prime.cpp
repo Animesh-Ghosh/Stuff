53e24ecf6a29a9ea032e22eade3d2b3928d82d50
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2018-11-17 19:35:50 +0530

    Update Prime.cpp
    Removed unnecessary content.

--- Prime/Prime.cpp
+++ Prime/Prime.cpp
@@ -5,16 +5,6 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-/*Eratosthenes' sieve algorithm from https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-1. Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
-2. Initially, let p equal 2, the smallest prime number.
-3. Enumerate the multiples of p by counting to n from 2p in increments of p, and mark them in the list 
-(these will be 2p, 3p, 4p, ...; the p itself should not be marked).
-4. Find the first number greater than p in the list that is not marked. If there was no such number, stop.
-Otherwise, let p now equal this new number (which is the next prime), and repeat from step 3.
-5. When the algorithm terminates, the numbers remaining not marked in the list are all the primes below n.
-*/
-
 int BinSearch (int &val, int Ar[], int N) {
 	int low = 0, high = N - 1, mid, flag = 0;
 	while (low <= high) {
