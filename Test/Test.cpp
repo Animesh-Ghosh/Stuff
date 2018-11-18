73b41e64d08067d9a19f137f3bd61ff864a1633a
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2018-11-18 20:24:36 +0530

    Uploaded files

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,29 +1,9 @@
-# include <iostream>
-# include <conio.h>
+#include <iostream>
 
-using std::cin;
-using std::cout;
-using std::endl;
+using namespace std;
 
-int BinSearch (int val, int Ar[], int N) {
-	int low = 0, high = N - 1, mid, flag = 0;
-	while (low <= high) {
-		mid = (low + high) / 2;
-		if (val < Ar[mid]) high = mid - 1;
-		else if (Ar[mid] < val) low = mid + 1;
-		else {
-			flag = 1;
-			break;
-		}
-	}
-	if (flag) return flag;
-	else return flag;
-}
-
-int main () {
-	int ar[] = {2};
-	if (BinSearch (2, ar, 1)) cout << "Found!" << endl;
-	else cout << "Not found!" << endl;
-	getch ();
+int main() {
+	int bytes = sizeof (int) * 100000;
+	cout << "MBs: " << (float) bytes / (1024 * 1024) << endl;
 	return 0;
 }
