d04b8e53bbfc4d96ce3ba5a9d770b6be15280bcd
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-02-21 22:54:12 +0530

    Update Fibonacci.cpp
    NEED TO MEMOIZE

--- Test/Fibonacci.cpp
+++ Test/Fibonacci.cpp
@@ -5,22 +5,14 @@ using std::cout;
 using std::endl;
 
 int Fibonacci (int N) {
-	static int *fib = new int[3];
-	fib[0] = -1;
+	int size = 3;
+	static int *fib = new int[size];
 	fib[1] = 0; fib[2] = 1;
-	int size = sizeof (fib))/sizeof (int);
-	if (N > size) {
-		int *ftemp = new int[2 * N];
-		ftemp[0] = -1;
-		for (int i = 1; i < 2 * N; i++) {
-			if (i < size) ftemp[i] = fib[i];
-			else ftemp[i] = 0;
-		}
-		
+	if (N == 1) return fib[1];
+	else if (N == 2) return fib[2];
+	else {
+		// memoize this shit
 	}
-	if (N == 1) return 0;
-	else if (N == 2) return 1;
-	return Fibonacci (N - 1) + Fibonacci (N - 2);
 }
 
 int main () {
