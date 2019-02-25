7718715b740c32792e0e6afc80be96245c1601d1
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-02-25 22:27:08 +0530

    Update Fibonacci.cpp

--- Test/Fibonacci.cpp
+++ Test/Fibonacci.cpp
@@ -7,8 +7,9 @@ using std::endl;
 long long Fibonacci (int N) {
     static long long fib[100] = {}; // initialises the array with all elements as 0
     fib[1] = 0; fib[2] = 1;
-	if (N == 1) return fib[1];
-	else if (N == 2) return fib[2];
+//	if (N == 1) return fib[1];
+//	else if (N == 2) return fib[2];
+	if (N == 1 || N == 2) return fib[N];
 	else if (fib[N] != 0) return fib[N];
 	else {
 	    fib[N] = Fibonacci (N - 1) + Fibonacci (N - 2);
@@ -21,6 +22,6 @@ int main () {
 	cout << "Enter number of terms: ";
 	cin >> n;
 	for (int i = 1; i <= n; i++) 
-		cout << Fibonacci (i) << " ";
+		cout << Fibonacci (i) << endl; // works correctly till i = 93
 	return 0;
 }
