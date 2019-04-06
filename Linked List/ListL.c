8a6bd0eaefd24dff899ecb381419429c31a53cc6
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-04-06 22:42:00 +0530

    Update ListL.c
    Can't understand the code for my life.

--- Linked List/ListL.c
+++ Linked List/ListL.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #define null NULL
 
-// linked list using triple ref
+// linked list using triple ref in vanilla fashion
 // Original code at http://www.eprg.org/computerphile/tripref.c
 
 struct NODE {
@@ -21,51 +21,71 @@ Node* Create (int data) {
 	return element;
 }
 
-void Insert (Node **head, Node* element) {
-	Node **current = head; /* double pointer so that 
-	start can be passed by "reference" */
+void Insert (Node **head, Node* element, int position) {
+	Node **tracer = head; /* double pointer holds address of start 
+	only points to the next pointers of the list */
+	int counter = 0;
 	if (*head == null) {
-		*head = element; /* value pointed to by head
-		viz. start will now point to element */
+		*head = element; /* *head points to element or 
+		start points to element */
+	}
+	else if (position == -1) {
+		/* while next of *tracer is not null or 
+		while next of current node is not null */
+		while ((*tracer)->next != null) {
+			tracer = &(*tracer)->next;
+			/* tracer holds address of next field of current node */
+			/* tracer = &(*tracer)->next; // right way
+			   *tracer = (*tracer)->next; // wrong way
+			   tracer = &((*tracer)->next); // same way */
+		}
+		/* finally, tracer holds address of next field of last node */
+		(*tracer)->next = element;
+		/* next of *tracer points to element or 
+		next of node holds address of element */
 	}
 	else {
-		/* while the value pointed to by current's 
-		next field ie. start->next is not NULL */
-		while ((*current)->next != null) {
-			current = &(*current)->next; // right way
-			// *current = (*current)->next; // wrong way
-			/* current is assigned the address of the 
-			next field of the value pointed to by current
-			ie. start->next */
+		while ((*tracer)->next != null && counter < position) {
+			tracer = &(*tracer)->next;
+			counter++;
 		}
-		(*current)->next = element;
-		/* value pointed to by current's next field 
-		now points to element */
+		element->next = (*tracer); // don't get it
+		(*tracer) = element; // this either
 	}
 }
 
 void Print (Node *head) {
 	Node *current = head;
 	while (current != null) {
-		printf("%d ", (current->data));
+		printf("%d ", current->data);
 		current = current->next;
 	}
 }
 
 int main (int argc, char const *argv[]) {
-	int i, size = atoi (argv[1]);
+	int i, size = atoi (argv[1]), value, position;
 	Node *current;
 	srand (time (0));
+	if (argc < 2) {
+		printf("Enter size of list: \n");
+		scanf ("%d", &size);
+	}
+	printf("Generating a list of random values...\n");
 	for (i = 0; i < size; i++) {
-		current = Create (rand () % size + 1);
-		Insert (&start, current); // passing start by "reference"
-		// printf ("%p\n", start);
+		current = Create (i);
+		Insert (&start, current, -1); // passing start by "reference"
 	}
 	printf("List: \n");
 	Print (start);
-	printf ("\nData at the start of list (hex): %p\n", *start);
+	printf ("\nValue of data at start of list(hex): %p\n", *start);
 	printf ("Address of start pointer: %p\n", &start);
 	printf ("Address of node at the start of list: %p\n", &*start);
-	printf ("Address of node at the start of list: %p", &(*start));
+	printf ("Address of node at the start of list: %p\n", &(*start));
+	printf ("Address of node at the start of list: %p\n", start);
+	printf ("Enter value to be inserted and it's position in the list: ");
+	scanf ("%d %d", &value, &position);
+	Insert (&start, Create (value), position);
+	printf ("Altered list: \n");
+	Print (start);
 	return 0;
 }
