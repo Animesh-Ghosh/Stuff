3c2516e22e2613132328a82a1802ba6e57457e07
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-04-23 17:22:26 +0530

    Update files
    Converting .cpps to .h for easier compilation.

--- ClearScreen/clearscreen.h
+++ ClearScreen/clearscreen.h
@@ -1,7 +1,24 @@
 // www.cplusplus.com/articles/4z18T05o
+
 #ifndef CLEARSCREEN_H
 	#define CLEARSCREEN_H
 
 	void ClearScreen();
 
+	#if defined (_WIN32) 
+
+		#define PLATFORM "Windows"
+		#include "clearscreen_windows.h"
+
+	#elif defined (__linux__)
+
+		#define PLATFORM "Linux"
+		#include "clearscreen_linux.h"
+
+	#endif
+
 #endif 
+
+void ClearScreen () {
+	__ClearScreen_PLATFORM ();
+}
---
+++ ClearScreen/clearscreen_old.h
@@ -0,0 +1,7 @@
+// www.cplusplus.com/articles/4z18T05o
+#ifndef CLEARSCREEN_H
+	#define CLEARSCREEN_H
+
+	void ClearScreen();
+
+#endif 
